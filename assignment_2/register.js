ba2052a4c4e95d93e6c98228ac62ae2e38cfcf86
Jirawat14 <natchjirawat@gmail.com>
2023-08-14 21:21:25 +0700

    assignment 1 & 2

---
+++ assignment_2/register.js
@@ -0,0 +1,34 @@
+const isValidToSubmit = () => {
+    let companyName = document.getElementById("companyName");
+    let contactPerson = document.getElementById("contactPerson");
+    let contactNumber = document.getElementById("contactNumber");
+    let chairAmount = document.getElementById("chairAmount");
+
+    companyName.classList.remove("invalid-input");
+    contactPerson.classList.remove("invalid-input");
+    contactNumber.classList.remove("invalid-input");
+    chairAmount.classList.remove("invalid-input");
+
+    if (companyName.value == "") {
+        alert("โปรดระบุชื่อบริษัทของคุณ (Please specify your company name)");
+        companyName.classList.add("invalid-input");
+        return false;
+    }
+    if (chairAmount.value == "" || chairAmount.value < 1 || chairAmount.value > 10) {
+        alert("โปรดระบุค่าตัวเลขตั้งแต่ 1 ถึง 10 (Please specify a number between 1 and 10)");
+        chairAmount.classList.add("invalid-input");
+        return false;
+    }
+    if (contactPerson.value == "") {
+        alert("โปรดระบุชื่อผู้ติดต่อ (Please specify your contact name)");
+        contactPerson.classList.add("invalid-input");
+        return false;
+    }
+    if (contactNumber.value == "") {
+        alert("โปรดระบุเบอร์โทรผู้ติดต่อ (Please specify your contact phone number)");
+        contactNumber.classList.add("invalid-input");
+        return false;
+    }
+    alert("ลงทะเบียนสำเร็จ !");
+    return true;
+}
